cd20ca04ea9f9a07628c808cba9aafe168d53975
Reyes Quiterio <300073318@fvtc.edu>
2023-04-12 10:33:54 -0500

    Hands on project 9

---
+++ Lesson_09/Ch09-Program/js09b.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+      
+      Eating Well in Season 
+      Author: Eliceo Reyes
+      Date: April 12, 2023  
+      
+      Filename: js09b.js
+ */
