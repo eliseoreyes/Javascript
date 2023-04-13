ff9832d21e8dc8ecab1c110a3936774de194aaca
Reyes Quiterio <300073318@fvtc.edu>
2023-04-13 11:30:16 -0500

    Chapter 9 Exercise

--- Lesson_09/Ch09-Program/js09b.js
+++ Lesson_09/Ch09-Program/js09b.js
@@ -7,7 +7,7 @@
       Author: Eliceo Reyes
       Date: April 12, 2023  
       
-      page 3734
+      page#382
 
       Filename: js09b.js 
  */
@@ -41,6 +41,21 @@
             inputBox.value = fieldValue;
             inputBox.disabled = true;
             document.getElementById('contactInfo').appendChild(inputBox);      
+      }
+
+      //Store data to local storage when the user signs up
+      document.getElementById('signupBtn').onclick = function(){
+            //Data fields to be saved to local storage
+             let formFields = document.querySelectorAll('#contactInfo input, input[type=radio], textarea');    
+             
+             //Write each field name and value to local storage
+             for (let fields of formFields){
+                  localStorage.setItem(fields.name, fields.value);
+             }
+
+             console.log(localStorage);
+
+             location.href = 'js09c.html';
       }
 
 
--- Lesson_09/Ch09-Program/js09c.js
+++ Lesson_09/Ch09-Program/js09c.js
@@ -10,3 +10,43 @@
       Filename: js09c.js 
  */
 
+      //Eating Well Preferences Keys
+      let keys = [
+                    'name'
+                    ,'email'
+                    ,'phone'
+                    ,'address'
+                    ,'city'
+                    ,'state'
+                    ,'zip'
+                    ,'allergies'
+                    ,'frequency'
+                    ,'size'
+                  ];
+
+      for (let item of keys) {      
+            let newRow = document.createElement('tr');
+            //Display the storage key
+            let keyCell = document.createElement('td');
+            keyCell.textContent = item;
+            newRow.appendChild(keyCell);
+
+            //Display the key value
+            let keyValue = document.createElement('td');
+            keyValue.textContent = localStorage.getItem(item);
+            newRow.appendChild(keyValue);
+
+            //Append each key=name pairs as a table row
+            document.getElementById('prefTable').appendChild(newRow);
+      }
+      
+      //Remove Eating Well keys when the Remove Preferences button in clicked
+      document.getElementById('removePrefBtn').onclick = function(){
+            
+            for (let item of keys) {
+                  localStorage.removeItem(item);
+            }
+
+            location.reload();
+      }
+
